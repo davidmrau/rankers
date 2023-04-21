dee8e70a5a39d1adbd01d7ad451d4295ab68c516
David Rau <david.rau@hotmail.de>
2023-04-21 18:07:41 +0200

    add plm dataset, update file_interface with json

--- file_interface.py
+++ file_interface.py
@@ -2,6 +2,40 @@ import numpy as np
 import gzip
 from tqdm import tqdm
 import subprocess
+
+
+import subprocess
+import json 
+class FileJson():
+    def __init__(self, fname, encoded=True):
+        self.file = {}
+        count_empty_docs = 0
+        n_lines = int(subprocess.check_output(f"wc -l {fname}", shell=True).decode().split(' ')[0])
+        with open(fname, 'r', encoding='utf-8') as f:
+            for line in tqdm(f, f'Loading file: {fname}', total=n_lines):
+                js = json.loads(line)
+                id_ = js['doc_id']
+                vector = js['plm']
+                ids, scores = [], []
+                for k in vector:
+                    ids.append(k)
+                    scores.append(vector[k])
+                self.file[id_] = (ids, scores)
+
+                
+    def __getitem__(self, id_):
+        if id_ not in self.file:    
+            print(f'"{id_}" not found!')
+            return None
+        return self.file[id_]
+
+    def __len__(self):
+        return len(self.file)
+
+
+
+
+
 class File():
     def __init__(self, fname, encoded=False):
         self.file = {}
